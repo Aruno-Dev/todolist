25c02b9d0100220be381b6850656e6f64157863f
arnaud terret <arnaud.terret@gmail.com>
2020-08-05 19:35:51 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './Todo'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+
+
+function App() {
+  return <div className="container">
+    <Todo />
+  </div>
+}
+
+export default App
