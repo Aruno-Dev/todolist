f28096ee1c2b05f59300a1916062839d82af4a5e
arnaud terret <arnaud.terret@gmail.com>
2020-08-05 20:09:50 +0200

    Updates

---
+++ precache-manifest.e733a6874ef46e4e6f5abce273d2f79a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ed0afb5b6f123e4f9d4177e8a24516",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d6b95c9db96d2847433c",
+    "url": "/todolist/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4a707ad919251c0bf507",
+    "url": "/todolist/static/css/main.07fcc481.chunk.css"
+  },
+  {
+    "revision": "d6b95c9db96d2847433c",
+    "url": "/todolist/static/js/2.bc06dcd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.bc06dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a707ad919251c0bf507",
+    "url": "/todolist/static/js/main.b259c234.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
